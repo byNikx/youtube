d9a37483333a0c361a3774f6047607aade52c1a2
byNikx <w3c.nadeem@hotmail.com>
2017-05-30 18:09:25 +0530

    1

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FileSelectDirective, FileDropDirective, FileUploader } from 'ng2-file-upload/ng2-file-upload';
 
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+
 @Component({
   selector: 'nx-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+	
+  public uploader:FileUploader = new FileUploader({url: URL});
 
   constructor() { }
 
