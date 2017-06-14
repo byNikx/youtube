812a532e0ded2f186223fe93094bcc7dd2098f79
byNikx <w3c.nadeem@hotmail.com>
2017-06-14 17:57:30 +0530

    1

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,7 +1,31 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
-import { FileUploader } from 'ng2-file-upload';
+import { Component, OnInit, DoCheck, Input } from '@angular/core';
+import { FileUploader, FileItem } from 'ng2-file-upload';
 
 const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+
+class FileStatus{
+  private status: string;
+  private progress: number;
+  
+  constructor() {}
+  
+  public setStatus(status: string): void{
+    this.status = status;
+  }
+
+  public setProgress(progress: number): void{
+    this.progress = progress;
+  }
+
+  public getStatus(): string{
+    return this.status;
+  }
+
+  public getProgres(): number{
+    return this.progress;
+  }
+
+}
 
 @Component({
   selector: 'nx-upload',
@@ -14,7 +38,16 @@ export class UploadComponent implements DoCheck {
   public uploader:FileUploader = new FileUploader({url: URL, autoUpload: true});
   public hasBaseDropZoneOver:boolean = false;
   public hasAnotherDropZoneOver:boolean = false;
+  @Input() file: FileItem = null;
+  public fileProgress: any = null;
  
+  constructor(){
+    this.fileProgress = this.getFormattedFileStatus(
+      this.getFileStatus(this.file)
+    );
+  }
+  ngOnInit() {
+  }
 
   ngDoCheck(){
   	console.log(this.uploader.queue);
@@ -27,6 +60,26 @@ export class UploadComponent implements DoCheck {
   public fileOverAnother(e:any):void {
     this.hasAnotherDropZoneOver = e;
   }
+
+  public getFileStatus(item: FileItem): FileStatus{
+    let fileStatus = new FileStatus();
+    if(item && item.isReady){
+      if(item.isUploading){
+        fileStatus.setProgress(item.progress);
+        fileStatus.setStatus('Uploading...');
+      }else{
+        fileStatus.setStatus('Waiting...');
+      }
+    }
+    return fileStatus;
+  }
+
+  public getFormattedFileStatus(fileStatus: FileStatus){
+    return {
+      label: `${fileStatus.getStatus()} ${fileStatus.getProgres()}%`
+    };
+  }
+
 
   files = [
   	{
@@ -56,9 +109,5 @@ export class UploadComponent implements DoCheck {
   	},
   ]
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
